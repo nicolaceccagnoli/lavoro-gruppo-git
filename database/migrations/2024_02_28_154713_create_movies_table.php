9cd0a4100b8f2001b9a2393b4bb81f2df096926f
DiegoFarrugio <diegofarrugio0@gmail.com>
2024-02-28 17:10:39 +0100

    Merge Diego

---
+++ database/migrations/2024_02_28_154713_create_movies_table.php
@@ -0,0 +1,32 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+return new class extends Migration
+{
+    /**
+     * Run the migrations.
+     */
+    public function up(): void
+    {
+        Schema::create('movies', function (Blueprint $table) {
+            $table->id();
+            $table->string('title', 64);
+            $table->string('original_title', 64);
+            $table->year('date');
+            $table->language('string', 64);
+            $table->duration('time');
+            $table->timestamps();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     */
+    public function down(): void
+    {
+        Schema::dropIfExists('movies');
+    }
+};
